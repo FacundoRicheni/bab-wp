e306d6739127ec0566be827b50b9639bb5f62b4e
facun <facundoricheni@gmail.com>
2022-10-21 04:19:53 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const imagenes = [
+    "../assets/img/altar.jpeg",
+    "../assets/img/deco_arbol.jpeg",
+    "../assets/img/mesa_plumas.jpeg",
+    "../assets/img/mesa_salada.jpeg",
+    "../assets/img/mesa2.jpeg",
+    "../assets/img/pergola.jpeg",
+    "../assets/img/pergola2.jpeg"
+];
